658dc008376cdbab005c856d71b63b2ae33294f4
James <heyjames@users.noreply.github.com>
2016-07-30 23:49:27 -0700

    Fixed grammar in main program description

--- parseStreamersFromHtml.py
+++ parseStreamersFromHtml.py
@@ -2,9 +2,9 @@ import re
 
 # This program extracts a string between [original-title="] and [">] without the
 # brackets, appends the extracted string onto a twitch.tv URL, and outputs it 
-# into a file named "links.html". This primarily useful if to create a list of 
-# selectable Twitch.tv profile links out of an HTML document taken from a Twitch
-# user's 'Following' page.
+# into a file named "links.html". This is primarily useful if to create a list 
+# of selectable Twitch.tv profile links out of an HTML document taken from a 
+# Twitch user's 'Following' page.
 
 # Opens fileWithHtmlCode.txt file in read mode. This is the file with the HTML 
 # code.
